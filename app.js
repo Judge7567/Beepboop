a39e3064f4899053b72bde46d54a534638f07452
The Senate <66320474+Judge7567@users.noreply.github.com>
2022-07-19 15:17:49 -0700

    messy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let button = document.querySelector("button")
 
 button.onclick = function(){
     let intro = introField.value
-    let message = "AMONGUS "
+    let message = "AMONGUS SUSSY "
     console.log(message)
     document.write(message)
 }
