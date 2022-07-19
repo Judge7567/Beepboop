769faefb3c2fc3fafb917fd2b03e24d8eed7c65e
The Senate <66320474+Judge7567@users.noreply.github.com>
2022-07-19 10:48:26 -0700

    fast

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("E")
+
+let introField = document.querySelector(".intro-field")
+let button = document.querySelector("button")
+
+button.onclick = function(){
+    let intro = introField.value
+    let message = "AMONGUS "
+    console.log(message)
+    document.write(message)
+}
