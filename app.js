af2899defb7272a0e3a602dccc01a9d36e074281
The Senate <66320474+Judge7567@users.noreply.github.com>
2022-07-20 12:53:12 -0700

    mmmmmesssssssssage

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ console.log("E")
 
 let introField = document.querySelector(".intro-field")
 let button = document.querySelector("button")
-let female = document.querySelector("female")
-let male = document.querySelector("male")
+const female = document.querySelector(".female")
+const male = document.querySelector(".male")
 button.onclick = function(){
     let intro = introField.value
     let message = "Memories broken The truth goes unspoken I’ve even forgotten my name I don’t know the season Or what is the reason I’m standing here holding my blade A desolate place Without any trace It’s only the cold wind I feel It’s me that I spite As I stand up and fight The only thing I know for real There will be blood-shed The man in the mirror nods his head The only one left Will ride upon the dragon’s back Because the mountains don’t give back what they take Oh no There will be blood-shed It’s the only thing I’ve ever known Losing my identity Wondering have I gone insane To find the truth in front of me I must climb this mountain rang Looking downward from this deadly height And never realizing why I fight"
@@ -12,9 +12,10 @@ button.onclick = function(){
 }
 
 female.onclick = function() {
-    const buttonMessage = document.querySelector('start-message')
+    const buttonMessage = document.querySelector('.start-message')
     buttonMessage.textContent = "santa cruz.  how you doin?"
-    buttonMessage.style["color"] = "green"
+    buttonMessage.style["background"] = "green"
+    buttonMessage.style["color"] = "white"
 }
 male.onclick = function() {
     const buttonMessage = document.querySelector('end-message')
